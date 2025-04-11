563e65fdd289a430c493839449b88cf97a315456
Mario <marianjakebula@gmail.com>
2025-04-12 01:33:20 +0800

    changed

--- final-project/src/Home.jsx
+++ final-project/src/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
                     <p class="lead fs-5 mb-2"><span className="text-success fw-bold">M </span>- MongoDB: NoSQL database where data is stored in flexible, JSON-like documents.</p>
                     <p class="lead fs-5 mb-2"><span className="text-danger fw-bold">E </span>- ExpressJS: A lightweight web application framework for Node.js used to build backend APIs.</p>
                     <p class="lead fs-5 mb-2"><span className="text-primary fw-bold">R </span>- ReactJS: A front-end JavaScript library developed by Facebook for building user interfaces.</p>
-                    <p class="lead fs-5 mb-5"><span className="text-info fw-boldgit ">N </span>- NodeJS: A runtime environment that allows you to run JavaScript on the server side.</p>
+                    <p class="lead fs-5 mb-5"><span className="text-info fw-boldgit">N </span>- NodeJS: A runtime environment that allows you to run JavaScript on the server side.</p>
                     <div>
                         <button type="button" class="btn btn-outline-dark btn-lg">Logout</button>
                     </div>
