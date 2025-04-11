547a5b93b5b35835c3dcda0e383013e3b00bf56e
Mario <marianjakebula@gmail.com>
2025-04-12 02:08:07 +0800

    added style but not yet finished on sign up page

--- final-project/src/components/Signup.jsx
+++ final-project/src/components/Signup.jsx
@@ -1,6 +1,24 @@
 const Signup = () => {
     return ( 
-        <h1>Sign up</h1>
+        <div className="container">
+            <form class="w-50 mx-auto">
+            <h1 class="lead fs-1 text-center mb-4">Sign up</h1>
+            <div class="form-group mb-4">
+                <label for="exampleInputEmail1">Name</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter Name"/>
+            </div>
+            <div class="form-group mb-4">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email"/>
+            </div>
+            <div class="form-group mb-4">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+            </div>
+            <button className="btn-block" type="submit" class="btn btn-outline-primary btn-lg w-100 mb-3">Sign up</button>
+            <small id="emailHelp" class="form-text">Already have an account? <a path="/login">Login here</a>.</small>
+            </form>
+        </div>
      );
 }
  
--- final-project/src/main.jsx
+++ final-project/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './index.css'
 import Home from './Home.jsx'
+import Signup from './components/Signup.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+      </Switch>
+    </Router>
 )
