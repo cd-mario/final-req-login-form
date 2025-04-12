3ff1fd298badf87d143898c9bae9470f1079f170
Mario <marianjakebula@gmail.com>
2025-04-12 22:56:24 +0800

    imported useState component for input data from the register page, implemented obsubmit and onchange methods

--- final-project/src/Home.jsx
+++ final-project/src/Home.jsx
@@ -2,19 +2,19 @@ const Home = () => {
     return ( 
         <>
             <nav class="navbar navbar-dark bg-dark justify-content-between p-4">
-                <a class="navbar-brand fs-4 rounded">Final Project</a>
-                <button type="button" class="btn btn-outline-light btn-lg">Logout</button>
+                <a class="navbar-brand fs-6 rounded">Final Project</a>
+                <button type="button" class="btn btn-outline-light">Logout</button>
             </nav>
             <div class="px-4 py-5 my-5 container">
                 <div className="card">
                     <div className="card-body">
-                            <h1 class="display-5 fw-bold text-body-emphasis text-dark">Welcome User!</h1>
+                            <h1 class="display-6">Welcome, User!</h1>
                             <p class="lead fs-5 mb-4">This is a register and login form made with MERN stack. For the final requirement</p>
                             <hr />
-                            <p class="lead fs-6 mb-2"><span className="text-success fw-bold">M </span>- MongoDB: NoSQL database where data is stored in flexible, JSON-like documents.</p>
-                            <p class="lead fs-6 mb-2"><span className="text-danger fw-bold">E </span>- ExpressJS: A lightweight web application framework for Node.js used to build backend APIs.</p>
-                            <p class="lead fs-6 mb-2"><span className="text-primary fw-bold">R </span>- ReactJS: A front-end JavaScript library developed by Facebook for building user interfaces.</p>
-                            <p class="lead fs-6 mb-5"><span className="text-info fw-bold">N </span>- NodeJS: A runtime environment that allows you to run JavaScript on the server side.</p>
+                            <p class="lead fs-6 mb-2"><span className="text-success fw-bold fs-5">M </span>- MongoDB: NoSQL database where data is stored in flexible, JSON-like documents.</p>
+                            <p class="lead fs-6 mb-2"><span className="text-danger fw-bold fs-5">E </span>- ExpressJS: A lightweight web application framework for Node.js used to build backend APIs.</p>
+                            <p class="lead fs-6 mb-2"><span className="text-primary fw-bold fs-5">R </span>- ReactJS: A front-end JavaScript library developed by Facebook for building user interfaces.</p>
+                            <p class="lead fs-6 mb-5"><span className="text-info fw-bold fs-5">N </span>- NodeJS: A runtime environment that allows you to run JavaScript on the server side.</p>
                     </div>
                 </div>
                 <hr />
--- final-project/src/components/Signup.jsx
+++ final-project/src/components/Signup.jsx
@@ -1,22 +1,41 @@
 import {Link} from 'react-router-dom'
+import {useState} from 'react'
 
 
 const Signup = () => {
+
+    const [data, setData] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const registerUser  = (e) => {
+        e.preventDefault()
+
+        // setData({
+        //     name: '',
+        //     email: '',
+        //     password: ''
+        // })
+
+    }
+
     return ( 
         <div className="container">
-            <form class="mx-auto rounded">
+            <form class="mx-auto rounded" onSubmit={registerUser}>
             <h1 class="lead fs-1 text-center mb-4 fw-bold text-primary">Sign up</h1>
             <hr />
             <div class="form-group mb-4 mt-4">
                 <label for="exampleInputEmail1" class="lead fs-6">Name</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter Name"/>
+                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter Name" value={data.name} onChange={(e) => setData({...data, name: e.target.value})}/>
             </div>
             <div class="form-group mb-4">
                 <label for="exampleInputEmail1" class="lead fs-6">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email"/>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" value={data.email} onChange={(e) => setData({...data, email: e.target.value})}/>
             </div>
             <div class="form-group mb-4">
-                <label for="exampleInputPassword1" class="lead fs-6">Password</label>
+                <label for="exampleInputPassword1" class="lead fs-6" value={data.password} onChange={(e) => setData({...data, password: e.target.value})}>Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
             </div>
             <button className="btn-block" type="submit" class="btn btn-outline-primary btn-lg w-100 mb-3">Sign up</button>
