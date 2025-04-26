d118ee8cf3aedd2e2554a423032f9acf4f900d9c
cd-mario <marianjakebula@gmail.com>
2025-04-27 05:09:09 +0800

    added image to the register page for more style

--- final-project/src/components/Signup.jsx
+++ final-project/src/components/Signup.jsx
@@ -2,7 +2,9 @@ import {Link} from 'react-router-dom'
 import {useState} from 'react'
 import axios from 'axios';
 import {toast} from 'react-hot-toast';
+import registerImg from '../images/register.jpg'
 import {useHistory} from 'react-router-dom';
+
 
 
 
@@ -36,31 +38,33 @@ const Signup = () => {
     }
 
     return ( 
-        <div className="container">
-            <form class="mx-auto rounded" onSubmit={registerUser}>
-            <h1 class="lead fs-1 text-center mb-4 fw-bold text-primary">Sign up</h1>
-            <hr />
-            <div class="form-group mb-4 mt-4">
-                <label class="lead fs-6">Name</label>
-                <input type="text" class="form-control"  placeholder="Enter Name" value={data.name} onChange={(e) => setData({...data, name: e.target.value})}/>
+        <div className="container" style={{width: '75%'}}>
+            <div class="row">
+                <div className="col">
+                    <img src={registerImg} class="mx-auto img-fluid mt-5" alt="Responsive image" />
+                </div>
+                <div class="col">
+                    <form class="mx-auto rounded p-4 mt-5" onSubmit={registerUser} style={{width: '90%'}}>
+                        <h1 class="display-5 text-center mb-5 fw-bold text-primary">Register</h1>
+                        <div class="form-group mb-4 mt-4">
+                            <input type="text" class="form-control lead"   placeholder="Name" value={data.name} onChange={(e) => setData({...data, name: e.target.value})}/>
+                        </div>
+                        <div class="form-group mb-4">
+                            <input type="email" class="form-control lead" placeholder="Email" value={data.email} onChange={(e) => setData({...data, email: e.target.value})}/>
+                        </div>
+                        <div class="form-group mb-4">
+                            <input type="text" class="form-control" placeholder="Address" value={data.address} onChange={(e) => setData({...data, address: e.target.value})}/>
+                        </div>
+                        <div class="form-group mb-4">
+                            <input type="password" class="form-control" placeholder="Password" value={data.password} onChange={(e) => setData({...data, password: e.target.value})}/>
+                        </div>
+                        <button className="btn-block" type="submit" class="btn btn-outline-primary btn-lg w-100 mb-3">Sign up</button>
+                        <div className="text-center">
+                        <small id="emailHelp" class="form-text">Already have an account?  <Link to="/login">Login here.</Link> </small>
+                        </div>
+                </form>
+                </div>
             </div>
-            <div class="form-group mb-4">
-                <label class="lead fs-6">Email</label>
-                <input type="email" class="form-control" placeholder="Enter email" value={data.email} onChange={(e) => setData({...data, email: e.target.value})}/>
-            </div>
-            <div class="form-group mb-4">
-                <label class="lead fs-6">Address</label>
-                <input type="text" class="form-control" placeholder="Enter address" value={data.address} onChange={(e) => setData({...data, address: e.target.value})}/>
-            </div>
-            <div class="form-group mb-4">
-                <label class="lead fs-6">password</label>
-                <input type="password" class="form-control" placeholder="Enter password" value={data.password} onChange={(e) => setData({...data, password: e.target.value})}/>
-            </div>
-            <button className="btn-block" type="submit" class="btn btn-outline-primary btn-lg w-100 mb-3">Sign up</button>
-            <div className="text-center">
-            <small id="emailHelp" class="form-text">Already have an account?  <Link to="/login">Login here.</Link> </small>
-            </div>
-            </form>
         </div>
      );
 }
